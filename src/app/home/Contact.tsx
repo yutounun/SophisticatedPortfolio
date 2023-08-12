73a5fe8d381bdfabec0eff36f91f65810281841a
yutounun <geek.yuto@gmail.com>
2023-08-12 11:21:41 +0900

    update contact message to add accounts and revise message

--- src/app/home/Contact.tsx
+++ src/app/home/Contact.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import AnimateInView from "../common/AnimateInView";
 import variants from "../common/Animation";
 import { MdMail } from "react-icons/md";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 import Impact from "./Impact";
 import Form from "./Form";
 import { IconContext } from "react-icons";
@@ -21,15 +26,15 @@ const Contact = () => {
               - CONTACT
             </p>
             <h2 className="mx-10 lg:mx-0 w-10/12 font-bold  text-primary tracking-wide text-4xl my-5">
-              We look forward to hearing your lovely message!
+              I am look forward to hearing your lovely message!
             </h2>
             <p className=" w-10/12 mx-10 lg:mx-0 text-[#6F7278] text-[18px] mb-5">
-              Would you like to find out more about us, do you have any
-              questions or suggestions? Please send us an email or use our
+              Would you like to find out more about me, do you have any
+              questions or suggestions? Please send me an email or use our
               contact form.
             </p>
             <div className="flex justify-center lg:inline-block lg:w-full">
-              <button className="flex rounded-full w-8/12 lg:w-7/12 h-16 bg-[#055f5b] text-xl text-white hover:bg-[#0c1c1b] mt-5 lg:flex gap-3 items-center">
+              <button className="flex rounded-full w-8/12 lg:w-6/12 h-16 bg-[#055f5b] text-xl text-white hover:bg-[#0c1c1b] mt-5 lg:flex gap-3 items-center">
                 <IconContext.Provider
                   value={{
                     color: "white",
@@ -45,6 +50,39 @@ const Contact = () => {
                   geek.yuto@gmail.com
                 </Link>
               </button>
+            </div>
+            {/* Accounts */}
+            <div className="flex gap-4 ml-5 mt-3">
+              <IconContext.Provider
+                value={{
+                  color: "#292c32",
+                  className: "text-2xl lg:text-4xl",
+                }}
+              >
+                <Link href="https://github.com/yutounun">
+                  <AiFillGithub />
+                </Link>
+              </IconContext.Provider>
+              <IconContext.Provider
+                value={{
+                  color: "#292c32",
+                  className: "text-2xl lg:text-4xl",
+                }}
+              >
+                <Link href="https://www.linkedin.com/in/yuto-ichihara-426800217/">
+                  <AiFillLinkedin />
+                </Link>
+              </IconContext.Provider>
+              <IconContext.Provider
+                value={{
+                  color: "#292c32",
+                  className: "text-2xl lg:text-4xl",
+                }}
+              >
+                <Link href="https://twitter.com/Robin_Ich_y">
+                  <AiFillTwitterCircle />
+                </Link>
+              </IconContext.Provider>
             </div>
           </div>
         </AnimateInView>
